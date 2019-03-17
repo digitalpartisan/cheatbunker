4d305d7061e44c122a1b5bf78a1b6ce8799d9901
digitalpartisan <digital.partisan.86@gmail.com>
2019-03-17 09:57:43 -0700

    Converted to Jiffy over individual helper scripts

--- .scaffold/config.sh
+++ .scaffold/config.sh
@@ -1,4 +1,4 @@
 SCAFFOLD_MOD_NAME="CheatBunker"
 SCAFFOLD_MOD_GAME_PATH="/k/Steam/steamapps/common/Fallout 4"
-SCAFFOLD_MOD_DELIVERABLES_PATH="/k/FO4 Mods/Packaging"
-SCAFFOLD_MOD_DEPENDENCIES="Loggout,Vault111ExitDetector,HolotapeRetainer,InjectTec,DynamicTerminal,Chronicle"
+SCAFFOLD_MOD_DELIVERABLES_PATH="/k/FO4 Modding/Packaging"
+SCAFFOLD_MOD_DEPENDENCIES="Jiffy,InjectTec,DynamicTerminal,Chronicle"
