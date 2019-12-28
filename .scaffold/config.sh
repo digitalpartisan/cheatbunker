440f7ecbfd928de4b2b0ac989fe2bdfc31720362
digitalpartisan <digital.partisan.86@gmail.com>
2019-12-28 09:50:43 -0800

    Updated scaffold config

--- .scaffold/config.sh
+++ .scaffold/config.sh
@@ -1,4 +1,2 @@
-SCAFFOLD_MOD_NAME="CheatBunker"
-SCAFFOLD_MOD_GAME_PATH="/k/Steam/steamapps/common/Fallout 4"
-SCAFFOLD_MOD_DELIVERABLES_PATH="/k/FO4 Modding/Packaging"
-SCAFFOLD_MOD_DEPENDENCIES="Jiffy,InjectTec,DynamicTerminal,Chronicle"
+SCAFFOLD_MOD="CheatBunker"
+SCAFFOLD_GAME="fallout4"
